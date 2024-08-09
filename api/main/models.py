cfc0bc15fc7b72c5e9122ec7b72126431318d39c
nlsnnn <lagged.06@mail.ru>
2024-08-09 13:15:24 +0300

    add Query model

--- api/main/models.py
+++ api/main/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Query(models.Model):
+    cad_num = models.CharField(max_length=100, verbose_name='Кадастровый номер')
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, verbose_name='Ширина')
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, verbose_name='Долгота')
+    result = models.BooleanField(null=True, blank=True, verbose_name='Ответ')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
